66f51bb047bf303288e8ba3fcefc6eb3c9acd352
Tomas Luken <tomas.luken@gmail.com>
2019-11-28 19:42:04 +0100

    voorbeeld van kleur aanpassen

---
+++ main.py
@@ -0,0 +1,8 @@
+from yeelight import Bulb
+import time
+bulb = Bulb('192.168.2.168')
+bulb.set_rgb(250, 100, 200)
+time.sleep(10)
+bulb.set_rgb(50, 50, 70)
+time.sleep(10)
+bulb.set_rgb(255, 80, 80)
