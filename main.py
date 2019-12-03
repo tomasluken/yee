988a9ef7b8e34b1e568a2c2d1f77ca9a3d6e80b4
Tomas Luken <tomas.luken@gmail.com>
2019-12-03 21:11:04 +0100

    new static ip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from yeelight import Bulb
 import time
-bulb = Bulb('192.168.2.168')
+bulb = Bulb('192.168.2.189')
 bulb.set_rgb(250, 100, 200)
 time.sleep(10)
 bulb.set_rgb(50, 50, 70)
--- school.py
+++ school.py
@@ -9,8 +9,9 @@ if len(sys.argv) == 3:
   hour = int(sys.argv[1])
   minute = int(sys.argv[2])
 
-if hour==8:
-  bulb = Bulb('192.168.2.168')
+bulb = Bulb('192.168.2.189')
+
+if hour==8: 
 
   if minute <= 5:
     print("green")
@@ -25,4 +26,16 @@ if hour==8:
   if minute <= 15:
     print("red")
     bulb.set_rgb(255, 0, 0)
+    quit()
 
+if hour==21:
+  bulb.set_rgb(255, 178, 102)
+  quit()
+
+if hour==22:
+  bulb.set_rgb(255, 128, 0)
+  quit()
+
+if hour==23:
+  bulb.set_rgb(255, 0, 0)
+  quit()
